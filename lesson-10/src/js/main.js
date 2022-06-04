797aaf5974cd6e9ccbdf5cfac75fd070f5943fa6
nma74028 <nma74028@gmail.com>
2022-06-04 22:59:38 +0300

    add

---
+++ lesson-10/src/js/main.js
@@ -0,0 +1,39 @@
+$(function(){
+	 $('.rev_slider').slick({
+         arrows: false,
+         dots: true,
+         responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]  
+         
+     });
+    });
+
+   
+ 
+    
+      
+      
+      
